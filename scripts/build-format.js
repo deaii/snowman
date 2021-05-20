70760122506d1f2f54d27ad1c2ff04a35616fddc
Daniel Alexander <dea.ii@live.com>
2021-05-20 00:32:50 -0700

    Snowman now builds in TS.  Next time, it will work.

--- scripts/build-format.js
+++ scripts/build-format.js
@@ -1,33 +1,34 @@
 var ejs = require('ejs');
-var exec = require('child-process-promise').exec;
-var fs = require('fs');
+var fs = require('fs/promises');
+var path = require('path');
 var pkg = require('../package.json');
-var shell = require('shelljs');
-const CleanCSS = require('clean-css');
 
 var encoding = { encoding: 'utf8' };
 
-/**
- * Read through the 'src' and combine and process CSS files
- *
- * @function buildCSS
- * @returns {void} Does not return value
- */
-function buildCSS () {
-  shell.rm('-f', 'lib/src/format.css');
-  shell.cat('lib/src/*.css').to('lib/src/format.css');
-  const file = fs.readFileSync('lib/src/format.css');
-  const output = new CleanCSS({ level: 2 }).minify(file);
-  shell.rm('-f', 'lib/src/format.css');
-  return output.styles;
-}
+const root = path.join(__dirname, '..');
+const buildPath = path.join(root, 'build');
+const srcPath = path.join(root, 'lib', 'src');
 
-Promise.all([
-  buildCSS(),
-  exec('browserify -g uglifyify lib/index.js -t [ babelify --presets [ @babel/env ] ]', { maxBuffer: Infinity })
-]).then(function (results) {
-  var distPath = 'dist/' + pkg.name.toLowerCase() + '-' + pkg.version;
-  var htmlTemplate = ejs.compile(fs.readFileSync('lib/src/index.ejs', encoding));
+const jsPath = path.join(buildPath, 'engine.js');
+const cssPath = path.join(buildPath, 'main.css');
+const distPath = path.join(root, 'dist', `${pkg.name.toLowerCase()}-${pkg.version}`);
+
+const ejsPath = path.join(srcPath, 'index.ejs');
+const svgPath = path.join(srcPath, 'icon.svg');
+
+const formatPath = path.join(distPath, 'format.js');
+const svgOutPath = path.join(distPath, 'icon.svg');
+
+async function doStuff() {
+  const [script, style, ejsFile] = await Promise.all([
+    fs.readFile(jsPath, encoding),
+    fs.readFile(cssPath, encoding),
+    fs.readFile(ejsPath, encoding),
+  ]);
+
+
+  var htmlTemplate = ejs.compile(ejsFile);
+
   var formatData = {
     author: pkg.author.replace(/ <.*>/, ''),
     description: pkg.description,
@@ -35,19 +36,23 @@ Promise.all([
     name: pkg.name,
     proofing: false,
     source: htmlTemplate({
-      style: results[0],
-      script: results[1].stdout
+      style: style,
+      script: script,
     }),
     url: pkg.repository,
     version: pkg.version
   };
 
-  shell.mkdir('-p', distPath);
+  try {
+    await fs.mkdir()
+  }
+  catch {
+  }
 
-  fs.writeFileSync(
-    distPath + '/format.js',
-    'window.storyFormat(' + JSON.stringify(formatData) + ');'
-  );
+  await Promise.all([
+    fs.writeFile(formatPath, `window.storyFormat(${JSON.stringify(formatData)});`),
+    fs.copyFile(svgPath, svgOutPath),
+  ]);
+}
 
-  shell.cp('lib/src/icon.svg', distPath + '/icon.svg');
-});
+doStuff();
